161bec3d783f537e59c40c624844f33f212243b5
Chris Mems <christopher.mems@.com>
2023-02-16 09:46:37 -0600

    filter through names

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
+import { useState } from 'react';
 import Data from './MOCK_DATA.json'
+import './App.css';
 
 function App() {
-
+  const [searchTerm, setSearchTerm] = useState('')
 
   return (
     <div className="App">
-      <input type="text" placeholder='Search...' />
-      {Data.map((val, index) => {
+      <input
+        type="text"
+        placeholder='Search...'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      {Data.filter((val) => {
+        if(searchTerm === ''){
+          return val
+        } else if(val.first_name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+          return val
+        }
+      }).map((val, index) => {
         return(
           <div className='user' key={index}>
             <p>{val.first_name}</p>
