431a6661509c121270d6bc34773814aa207707c4
Chris Mems <christopher.mems@.com>
2023-02-16 09:21:11 -0600

    map through data to display first names

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Data from './MOCK_DATA.json'
 
 function App() {
+
+
   return (
     <div className="App">
-      vdv
+      <input type="text" placeholder='Search...' />
+      {Data.map((val, index) => {
+        return <div key={index}>{val.first_name}</div>
+      })}
     </div>
   );
 }
